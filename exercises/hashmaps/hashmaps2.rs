ce969b596ed0099f1464fae70a77f3ad518d9ab0
yukinorong <863347350@qq.com>
2023-03-13 15:24:21 +0000

    3.12 sixth commit

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -11,7 +11,6 @@
 //
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -37,6 +36,15 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
+        match fruit {
+            Fruit::Banana => {
+                basket.insert(fruit, 10);
+            }
+            Fruit::Pineapple => {
+                basket.insert(fruit, 10);
+            }
+            _ => {}
+        }
     }
 }
 
