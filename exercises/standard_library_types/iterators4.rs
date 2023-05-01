f345692afca859ad187350eb815d616551793f90
yukinorong <863347350@qq.com>
2023-05-01 09:15:36 +0000

    5.1

--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -1,7 +1,6 @@
 // iterators4.rs
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
@@ -13,6 +12,9 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+
+    (1..=num).collect::<Vec<u64>>().iter().fold(1, |acc, x| acc * x)
+    // (1..=num).product()
 }
 
 #[cfg(test)]
